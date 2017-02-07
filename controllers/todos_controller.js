6316e814596ac062ad4e516a97819b3ddfd44da2
andre the <andre.the12@gmail.com>
2017-02-07 10:28:22 +0800

    edited functions

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -13,24 +13,83 @@ const todos = []
 
 // CREATE - params should be an object with keys for name, description and completed
 function create (params) {
+  if(!params.name)throw new Error('Name is required')
+  if(!params.name.length < 5) throw new Error('Name is too short')
+  if(!params.description)params.description = 'My new todo list'
+  if(!params.completed)params.completed =false
+
+  params._id =uuidGenerator()
+  todos.push(params)
 }
 
+  if (typeof params === 'object' && params.name.length >= 5) {
+      todos.push({
+        _id: uuidGenerator(),
+        name: params.name,
+        description: params.description || params.name,
+        completed: params.completed || false
+      })
+    } else {
+      return false
+}
+}
 // READ (list & show)
 function list () {
   // return list of all TODOs
+  return todos
 }
 function show (id) {
   // find the TODO with this id
-}
+  var allTodos=list()
+  for (var i = 0; i < allTodos.length; i++) {
+    if(allTodos[i]._id===id) return allTodos[i]
+  }
+  return null;
+
+//   allTodos=allTodos.filter(function(todo){
+//     if(todo._id === id) return todo
+//   })
+//   return(allTodos[0]) ? allTodos[0]:null
+// }
 
 // UPDATE - params should be an object with KVPs for the fields to update
 function update (id, params) {
-}
+  var todoById = show(id)
+  if(!params.name)return false
+  if(params.name.length < 5)return false
+
+  for(var key in params){
+    if(todoById.hasOwnProperty(key)){
+      todoById[key] =params[key]
+      }
+    }
+    return true
+  }
 
 // DESTROY (destroy & destroyAll)
 function destroy (id) {
+  var todoByid= show(id)
+  var deletedIndex = todos.indexof(todoById)
+
+  if(!todoById) return false
+  if(todo.splice(deletedIndex, 1))return true
 }
 
+//   if(todos['_id']===id){
+//     for(var i=0; i<todos.length;i++){
+//       if(todos[i]['_id']===id){
+//         todos.splice(i,1)
+//         return true
+//       }
+//     }
+//   }else return false
+// }
+function destroyAll(){
+  todos.forEach(function(todo){
+    destroy(todo._id)
+  })
+  return true
+}
 module.exports = {
   create,
   list,
--- test/todo_tests.js
+++ test/todo_tests.js
@@ -2,4 +2,48 @@ const assert = require('assert')
 const todos = require('../controllers/todos_controller.js')
 
 // // Use Assert to Test the functionality of all your CRUD methods e.g.
-// assert.strictEqual(todos.list().length, 0, 'List should return an array of all todos')
+
+// create
+var todoItem1={
+  name: 'Take out the trash',
+  description: 'trash',
+  completed: true
+}
+var todoItem2={
+  name:
+  description:
+  Completed:
+}
+var todoItem3={
+  description:
+  Completed:
+}
+
+assert.strictEqual(todos.create(todoItem1), true, 'Should return true')
+assert.strictEqual(todos.create(todoItem2), false, 'Function create, object not create should have at least 5 length')
+assert.strictEqual(todos.create(todoItem3), false, '')
+
+// var todo = todos.list()[0]
+// assert.strictEqual(todos.list().length, 1, 'params should be an object with keys for name, description and completed')
+// assert.strictEqual(todo['name'], 'Take out the trash', 'Todo object should have a "name" property')
+// assert.strictEqual(todo['description'], 'trash', 'Todo object should have a "description" property')
+// assert.strictEqual(todo['completed'], true, 'Todo object should have a "completed" property')
+// assert.strictEqual(todos.create({'name': 'dick'}), 'name is less than 5 characters, terminating process', 'constructor was denied from creating an object')
+// assert.ok(todos.list()[0].hasOwnProperty('_id'), 'Should automatically create an _id property for each new TODO and assign it a UUID')
+// // READ
+
+// list
+assert.strictEqual(todos.list().length, 0, 'List should return an array of all todos')
+
+// //show
+var checkId =todo.list()[0]._id
+assert.strictEqual(todos.show(checkId), todos.list()[0], 'Show should return Todo object with specified id')
+assert.strictEqual(todos.show('nonexistingID'), null, 'Should return null if no TODO with that id exists')
+
+// update
+
+
+
+//destroy
+
+//Destroy(id)
